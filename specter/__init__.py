15d9edc246757bb17bc5e657f772caf64c19ddef
PidgeyL <pieterjan.moreels@gmail.com>
2016-03-16 18:10:11 +0100

    fix typo

--- specter/__init__.py
+++ specter/__init__.py
@@ -12,7 +12,7 @@
 
 from .specter import Specter
 
-__copyright__ = 'Copyright 2016 by MoreelsMoreels'
+__copyright__ = 'Copyright 2016 by Moreels Pieter-Jan'
 __version__ = '0.1.0'
 __license__ = 'BSD'
 __author__ = 'Moreels Pieter-Jan'
